db9a623557ba2cdbf2ed4fd9cf56c8d831844d22
devononymous <suloksushil@gmail.com>
2025-04-15 20:23:45 +0530

    intialized

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SplashCursor from './components/utils/SplashCurosr' // Uncomment if needed and ensure the module exists
+
+const App: React.FC = () => {
+  return (
+    <div>App
+      <SplashCursor />
+
+    </div>
+  )
+}
+
+export default App
